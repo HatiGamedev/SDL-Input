6bec493b96a189c9f93902b51706be2f51b2d133
Hati Eth <hati.eth@gmail.com>
2014-11-11 17:35:26 +0100

    better stub

--- sdlinputwrapper/stub.h
+++ sdlinputwrapper/stub.h
@@ -2,9 +2,9 @@
 #define STUB_H
 
 #ifdef __llvm__
-#   define STUB(X) {static bool STUBBED=false; if(!STUBBED) { STUBBED=true; std::printf("[%s] %s:%s\n", __FILE__, __PRETTY_FUNCTION__, #X); }}
+#   define STUB(X) {static bool STUBBED=false; if(!STUBBED) { STUBBED=true; std::printf("STUB[%s:%d] %s:%s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, #X); }}
 #elif __GNUC__
-#   define STUB(X) {static bool STUBBED=false; if(!STUBBED) { STUBBED=true; std::printf("[%s] %s:%s\n", __FILE__, __PRETTY_FUNCTION__, #X); }}
+#   define STUB(X) {static bool STUBBED=false; if(!STUBBED) { STUBBED=true; std::printf("STUB[%s:%d] %s:%s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, #X); }}
 #else
 #   define STUB(X) (void)0
 #endif
